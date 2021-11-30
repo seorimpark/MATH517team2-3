989a20b0b81ea0bc2109708f575bd5ab039f80b6
salimajaoua <salima.jaoua@epfl.ch>
2021-11-30 18:41:54 +0100

    Create predictCNTandBA.R

---
+++ Code/predictCNTandBA.R
@@ -0,0 +1,33 @@
+library(ggplot2)
+library(tidyr)
+library(dplyr)
+library(corrplot)
+library(maps)
+library(tidyverse)
+library(Hmisc)
+library(zoo)
+library(hrbrthemes) 
+library(pscl)
+library('plot.matrix')
+library('psych')
+library(bookdown)
+library(quantreg)
+library(latex2exp)
+library(cowplot)
+library(reshape2)
+library(stargazer)
+library(knitr)
+library(stargazer)
+load ("../Data/data_train_DF.RData")
+df <- data_train_DF
+#Remove the NA values from the data
+train<-df[!is.na(df$CNT),] 
+train<-df[!is.na(df$BA),]
+test <- df[is.na(df$BA),]
+test <- df[is.na(df$CNT),]
+colnames(train)
+m1<-zeroinfl(CNT~lc1+lc2+lc3+lc4+lc5+lc6+lc7+lc8+lc9+lc10+
+               lc11+lc12+lc13+lc14+lc15+lc16+lc17+lc18+
+               clim1+clim2+clim3+clim4+ clim5+ clim6+ clim7+clim8+
+               clim10+ altiMean+ altiSD, data = train)
+
