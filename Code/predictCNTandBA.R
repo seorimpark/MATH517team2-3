2d782604c4d80cf96181dc3d0faef0ab3456cb38
salimajaoua <salima.jaoua@epfl.ch>
2021-12-02 20:37:11 +0100

    Update predictCNTandBA.R

--- Code/predictCNTandBA.R
+++ Code/predictCNTandBA.R
@@ -30,4 +30,19 @@ m1<-zeroinfl(CNT~lc1+lc2+lc3+lc4+lc5+lc6+lc7+lc8+lc9+lc10+
                lc11+lc12+lc13+lc14+lc15+lc16+lc17+lc18+
                clim1+clim2+clim3+clim4+ clim5+ clim6+ clim7+clim8+
                clim10+ altiMean+ altiSD, data = train)
+m2 <-zeroinfl(BA~lc1+lc2+lc3+lc4+lc5+lc6+lc7+lc8+lc9+lc10+
+                lc11+lc12+lc13+lc14+lc15+lc16+lc17+lc18+
+                clim1+clim2+clim3+clim4+ clim5+ clim6+ clim7+clim8+
+                clim10+ altiMean+ altiSD, data = train)
+#takes too much times to compute 
+step(m1, direction = "backward")
+#step(m2, direction = "backward", trace = FALSE)
+#Need to think about something that makes it run faster 
+#PREDICTION : 
 
+#add the plot of the other file and we need to compute the accuracy of our model : 
+
+
+#add the missing value 
+
+#plot the distribution of the missing value 
